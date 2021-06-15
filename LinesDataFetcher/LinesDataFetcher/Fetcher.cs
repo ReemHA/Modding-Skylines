93f91b5b8d0bf3342e08b91fd5098826f173e5ec
Reem Abdelrazek <reem.elrazek@gmail.com>
2021-06-15 11:10:18 +0200

    using wkt

--- LinesDataFetcher/LinesDataFetcher/Fetcher.cs
+++ LinesDataFetcher/LinesDataFetcher/Fetcher.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using ICities;
 using Leguar.TotalJSON;
@@ -6,6 +7,31 @@ using UnityEngine;
 
 namespace LinesDataFetcher
 {
+    class LineLonLat
+    {
+        public double st_lon;
+        public double st_lat;
+        public double end_lon;
+        public double end_lat;
+
+        public LineLonLat(double st_lon, double st_lat, double end_lon, double end_lat)
+        {
+            this.st_lon = st_lon;
+            this.st_lat = st_lat;
+            this.end_lon = end_lon;
+            this.end_lat = end_lat;
+        }
+    }
+
+
+    class SegmentData
+    {
+        public LineLonLat line;
+        public int traffic_buffer;
+        public int traffic_density;
+    }
+
+
     public class Fetcher : ThreadingExtensionBase
     {
         public override void OnAfterSimulationFrame()
@@ -85,7 +111,42 @@ namespace LinesDataFetcher
                         json.Add("position", JSON.Serialize(node.m_position));
                         JArray segArray = new JArray();
                         JSON temp = new JSON();
-                        AddSegmentsToArray(segments, node, segArray);
+                        temp = AddSegmentToJson(node.m_segment1, segments);
+                        if (temp != null)
+                        {
+                            segArray.Add(temp);
+                        }
+                        temp = AddSegmentToJson(node.m_segment2, segments);
+                        if (temp != null)
+                        {
+                            segArray.Add(temp);
+                        }
+                        temp = AddSegmentToJson(node.m_segment3, segments);
+                        if (temp != null)
+                        {
+                            segArray.Add(temp);
+                        }
+                        temp = AddSegmentToJson(node.m_segment4, segments);
+                        if (temp != null)
+                        {
+                            segArray.Add(temp);
+                        }
+                        temp = AddSegmentToJson(node.m_segment5, segments);
+                        if (temp != null)
+                        {
+                            segArray.Add(temp);
+                        }
+                        temp = AddSegmentToJson(node.m_segment6, segments);
+                        if (temp != null)
+                        {
+                            segArray.Add(temp);
+                        }
+                        temp = AddSegmentToJson(node.m_segment7, segments);
+                        if (temp != null)
+                        {
+                            segArray.Add(temp);
+                        }
+                        json.Add("segments data", segArray);
                         jArray.Add(json);
                     }
                 }
@@ -95,7 +156,63 @@ namespace LinesDataFetcher
                 outputFile.WriteLine(txtLines);
                 outputFile.Close();
             }
+            else if (Input.GetKeyDown(KeyCode.RightAlt))
+            {
+                StringBuilder txtLines = new StringBuilder();
+                NetNode[] nodes = NetManager.instance.m_nodes.m_buffer;
+                NetSegment[] segments = NetManager.instance.m_segments.m_buffer;
+                ushort nodeId;
+                txtLines.Append("Node ID;Line;Traffic Buffer;Traffic Density\n");
+                foreach (var node in nodes)
+                {
+                    nodeId = node.m_infoIndex;
+                    //if (node.m_flags.ToString().Contains(NetNode.Flags.Junction.ToString()) &&
+                    //    !(node.m_flags.ToString().Contains(NetNode.Flags.OneWayIn.ToString()) &&
+                    //    node.m_flags.ToString().Contains(NetNode.Flags.OneWayOut.ToString())))
+                    //{
+                        SegmentData temp = GetSegmentData(node.m_segment1, segments, nodes);
+                        if (temp != null)
+                        {
+                            AddSegmentDataToWKT(temp,  txtLines, nodeId);
+                        }
+                        temp = GetSegmentData(node.m_segment2, segments, nodes);
+                        if (temp != null)
+                        {
+                            AddSegmentDataToWKT(temp,  txtLines, nodeId);
+                        }
+                        temp = GetSegmentData(node.m_segment3, segments, nodes);
+                        if (temp != null)
+                        {
+                            AddSegmentDataToWKT(temp,  txtLines, nodeId);
+                        }
+                        temp = GetSegmentData(node.m_segment4, segments, nodes);
+                        if (temp != null)
+                        {
+                            AddSegmentDataToWKT(temp,  txtLines, nodeId);
+                        }
+                        temp = GetSegmentData(node.m_segment5, segments, nodes);
+                        if (temp != null)
+                        {
+                            AddSegmentDataToWKT(temp,  txtLines, nodeId);
+                        }
+                        temp = GetSegmentData(node.m_segment6, segments, nodes);
+                        if (temp != null)
+                        {
+                            AddSegmentDataToWKT(temp,  txtLines, nodeId);
+                        }
+                        temp = GetSegmentData(node.m_segment7, segments, nodes);
+                        if (temp != null)
+                        {
+                            AddSegmentDataToWKT(temp,  txtLines, nodeId);
+                        }
+                   //}
+                }
+                StreamWriter outputFile = new StreamWriter("Files/traffic_data.wkt");
+                outputFile.WriteLine(txtLines);
+                outputFile.Close();
+            }
         }
+
 
         private void AddSegmentsToArray(NetSegment[] segments, NetNode node, JArray segArray)
         {
@@ -149,5 +266,54 @@ namespace LinesDataFetcher
             }
             return null;
         }
+
+        private LineLonLat SegmentToLine(NetNode st_node, NetNode end_node)
+        {
+            // segment start point to lon and lat
+            var st_point = st_node.m_position;
+            var st_lon = GetLonLat(st_point.x, -8639.736, 8639.736, 121.3931, 121.5786);
+            var st_lat = GetLonLat(st_point.z, -8639.736, 8639.736, 31.1576, 31.3137);
+
+            // segment end point to lon and lat
+            var end_point = end_node.m_position;
+            var end_lon = GetLonLat(end_point.x, -8639.736, 8639.736, 121.3931, 121.5786);
+            var end_lat = GetLonLat(end_point.z, -8639.736, 8639.736, 31.1576, 31.3137);
+            Debug.Log(st_lon + " " + st_lat + " " + end_lon + " " + end_lat);
+            return new LineLonLat(st_lon, st_lat, end_lon, end_lat);
+        }
+
+        private SegmentData GetSegmentData(ushort index, NetSegment[] segments, NetNode[] nodes)
+        {
+            if (index != 0)
+            {
+                SegmentData segmentData = new SegmentData();
+                LineLonLat line = SegmentToLine(nodes[segments[index].m_startNode], nodes[segments[index].m_endNode]);
+                segmentData.line = line;
+                segmentData.traffic_buffer = segments[index].m_trafficBuffer;
+                segmentData.traffic_density = segments[index].m_trafficDensity;
+                return segmentData;
+            }
+            return null;
+        }
+
+        private void AddSegmentDataToWKT(SegmentData temp, StringBuilder txtLines, ushort node_id)
+        {
+            txtLines.Append(node_id + ";LINESTRING(" +
+                temp.line.st_lon + " " +
+                temp.line.st_lat + ", " +
+                temp.line.end_lon + " " +
+                temp.line.end_lat + ");" +
+                temp.traffic_buffer + ";" +
+                temp.traffic_density + "\n");
+            Debug.Log(txtLines.ToString() + "HHHHHHH");
+
+        }
+
+        private double GetLonLat(double number, double inMin, double inMax, double outMin, double outMax)
+        {
+            return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+        }
+
+
     }
 }
